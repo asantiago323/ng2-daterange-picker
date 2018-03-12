693ada9fee22ce57781bd9a551be504279450d66
Anthony Santiago <Anthony.Santiago@caci.com>
2018-03-12 09:27:07 -0400

    minor material tweaks

--- src/daterange.picker.module.ts
+++ src/daterange.picker.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
-import { MaterialModule } from '@angular/material';
+import { MatButtonModule, MatInputModule } from '@angular/material';
 
 
 import { DaterangePickerComponent } from './ang.daterange.picker.component';
@@ -13,6 +13,6 @@ export * from './ang.daterange.picker.component';
 @NgModule({
   declarations: [ DaterangePickerComponent, DatePickerComponent ],
   exports: [ DaterangePickerComponent ],
-  imports: [ CommonModule, FormsModule, HttpModule, ReactiveFormsModule, MaterialModule ]
+  imports: [ CommonModule, FormsModule, HttpModule, ReactiveFormsModule, MatButtonModule, MatInputModule ]
 })
 export class DaterangePickerModule { }
